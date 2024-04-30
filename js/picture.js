f0ce30c0fb10a29e510d18391aec0e2a50c2b2f5
Singing-Shadow <Yao20140121@outlook.com>
2024-04-30 20:19:19 +0800

    Update picture.js

--- js/picture.js
+++ js/picture.js
@@ -3,8 +3,5 @@ const pictureFolder = "https://singing-shadow.github.io/picture";
 
 // 获取包含图片文件的文件夹
 fetch(pictureFolder)
-    .then(response => response.text())
-    .then((data) => console.log(data))
-    .catch(error => {
-        console.error("Error fetching images: ", error);
-    });
+    .then((response) => response.text())
+    .then((data) => window.alert(data));
