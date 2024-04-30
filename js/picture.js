0a07a42913f8c2da2c180eb3d45e502005193067
Singing-Shadow <Yao20140121@outlook.com>
2024-04-30 20:13:46 +0800

    js
    picture

--- js/picture.js
+++ js/picture.js
@@ -1,12 +1,10 @@
-document.addEventListener("DOMContentLoaded", function() {
-    // 获取图片文件夹路径
-    const pictureFolder = "https://singing-shadow.github.io/picture";
+// 获取图片文件夹路径
+const pictureFolder = "https://singing-shadow.github.io/picture";
 
-    // 获取包含图片文件的文件夹
-    fetch(pictureFolder)
-        .then(response => response.text())
-        .then((data) => console.log(data))
-        .catch(error => {
-            console.error("Error fetching images: ", error);
-        });
-});
+// 获取包含图片文件的文件夹
+fetch(pictureFolder)
+    .then(response => response.text())
+    .then((data) => console.log(data))
+    .catch(error => {
+        console.error("Error fetching images: ", error);
+    });
