9f298d7188b50dd53af12048684bfd2436195ac8
Singing-Shadow <Yao20140121@outlook.com>
2024-04-30 22:15:25 +0800

    for...

--- js/gallery.js
+++ js/gallery.js
@@ -1,7 +1,38 @@
 // 获取图片文件夹路径
-const pictureFolder = "https://singing-shadow.github.io/picture";
+const pictureFolder = [
+    "picture/Arcaea_光.jpg"
+  , "picture/Arcaea_光2.jpg"
+  , "picture/Arcaea_光和对立.jpg"
+  , "/picture/JK 光＆对立.jpg"
+  , "picture/Kou (Arcaea).jpg"
+  , "picture/Kou.jpg"
+  , "picture/pixivFANBOX.png"
+  , "picture/Pokémon盖欧卡.jpg"
+  , "picture/rkgk.png"
+  , "picture/SHE - BLACK RAT 米山舞.jpg"
+  , "picture/カノンとラティアス.png"
+  , "picture/バニー紅ちゃん.jpg"
+  , "picture/ポケモン25周年.jpg"
+  , "picture/ラティアス & ラティオス.png"
+  , "picture/或る神話の消えた日.jpg"
+  , "picture/花对立.jpg"
+];
 
-// 获取包含图片文件的文件夹
-fetch(pictureFolder)
-    .then((response) => response.url())
-    .then((data) => console.log(data));
+for (let i = 0; i < pictureFolder.length; i++) {
+  // 创建图片链接元素
+  const linkElement = document.createElement("a");
+  linkElement.href = pictureFolder[i];
+  linkElement.alt = "图片" + (i + 1);
+  linkElement.target = "_blank";
+
+  // 创建图片元素
+  const imgElement = document.createElement("img");
+  imgElement.src = pictureFolder[i];
+  imgElement.alt = "图片" + (i + 1);
+
+  // 将图片元素添加到链接元素中
+  linkElement.appendChild(imgElement);
+
+  // 将链接元素添加到图片展示区域的容器元素中
+  document.getElementById("image").appendChild(linkElement).appendChild(imgElement);
+}
