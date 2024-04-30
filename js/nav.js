d5dbd8d6b3eb886e94180af7676a908a181906e6
Singing-Shadow <Yao20140121@outlook.com>
2024-04-30 19:04:17 +0800

    代码复用测试
    代码复用测试

---
+++ js/nav.js
@@ -0,0 +1,29 @@
+document.getElementById("nav").innerHTML = `
+    <ul class="numberList">
+        <li class="number">
+            <a href="index.html">首页</a>
+        </li>
+        <li class="number">
+            分类
+            <ul class="menu">
+                <li> <a href="picture.html">我的图廊</a> </li>
+                <li> <a href="https://www.pixiv.net">pixiv</a> </li>
+                <li> <a href="">教程</a> </li>
+                <li> <a href="">景物</a> </li>
+                <li> <a href="">人物</a> </li>
+                <li> <a href="">其它</a> </li>
+            </ul>
+        </li>
+        <li class="number">
+            我的
+            <ul class="menu">
+                <li> <a href="https://github.com/Singing-Shadow">我的GitHub</a> </li>
+                <li> <a href="https://dylanyu233.rbind.io/">好友博客</a> </li>
+                <li> <a href="https://github.com/MrRightXingYue">好友GitHub</a> </li>
+                <li> <a href="">历史记录</a> </li>
+                <li> <a href="">订单管理</a> </li>
+                <li> <a href="">设置中心</a> </li>
+            </ul>
+        </li>
+    </ul>
+`
